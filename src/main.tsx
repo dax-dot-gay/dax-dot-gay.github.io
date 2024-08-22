33c744c03057ce2aaeb56ff8bb12e169699ca19c
Dax Harris <mvh3875@rit.edu>
2024-08-22 11:32:18 -0400

    base setup

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from "react-dom/client";
+import { App } from "./App.tsx";
+import "@mantine/core/styles.css";
+import "@mantine/core/styles.css";
+import "@mantine/spotlight/styles.css";
+
+createRoot(document.getElementById("root")!).render(<App />);
